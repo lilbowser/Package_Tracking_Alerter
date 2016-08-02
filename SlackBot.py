05780b7ec18d33dd8a5a499dae1dd94302718fde
Ashley <Ashley@x303.com>
2016-08-02 09:25:18 -0400

    Pull slacker token from config file instead of being hard coded (Whoops)

--- SlackBot.py
+++ SlackBot.py
@@ -6,15 +6,26 @@ Slacker Test Code
 """
 
 from slacker import Slacker
+import yaml
+import datetime
 
-slack = Slacker('xoxp-64732089472-64729067860-64686766979-5df7efceba')
 
-# Send a message to #general channel
-slack.chat.post_message('#general', 'Hello fellow slackers!')
 
-# Get users list
-response = slack.users.list()
-users = response.body['members']
-print(users)
-# Upload a file
-# slack.files.upload('hello.txt')
+def load_config(config_file_name):
+    with open(config_file_name) as config:
+        return yaml.load(config)["api_keys"]
+
+if __name__ == '__main__':
+
+    config = load_config('secrets.yaml')
+    slack = Slacker(config['slack']['key'])
+
+    # Send a message to #general channel
+    slack.chat.post_message('#general', 'Hello fellow slackers! The current time is {}.'.format(datetime.datetime.now()))
+
+    # Get users list
+    response = slack.users.list()
+    users = response.body['members']
+    print(users)
+    # Upload a file
+    # slack.files.upload('hello.txt')
